5d6b872c9c085f9199cf563d619720b1c9ba2de7
Yatinah <atinn433@gmail.com>
2021-12-03 19:42:11 +0700

    dashboard

---
+++ components/Header2.js
@@ -0,0 +1,46 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+
+function Header2(){
+  function userbin(){
+
+  }
+    return(
+      <>
+      <div className="headdash">
+        <div className='navbardash'>
+            <div className="header">
+              <h1>Unjaya.id</h1>
+            </div>
+            <div className="headernya">
+              <div className ="dashb">
+                <h2>Dashboard</h2>
+              </div>
+              <div className="akun">
+                <Image src="/Group.png" alt="akun" width={23} height={23}/>
+              </div>
+              <div className="poly">
+                <Image src="/polygon.png" onClick={userbin} alt="poly" width={25} height={8}/>
+                <div className="user">
+                  {/* <h3>@Userbin</h3>
+                  <p>Keluar</p> */}
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
+  </>
+    // <div className="headernya">
+  
+    //     <div className="header">
+    //       <h1>Unjaya.id</h1>
+    //       <h2>Dashboard</h2>
+    //     </div>
+    //     <Link href='/'>
+    //     <a class="dashboard">dashboard</a> 
+    //     </Link>
+    // </div>
+  )
+}
+export default Header2
