ca245c6e8f61d0450a587081e890d79c17dc6917
Atiinn <80826110+atinnna@users.noreply.github.com>
2021-12-03 21:53:19 +0700

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,10 @@
 import '../styles/globals.css'
 import '../styles/layout.css'
-<<<<<<< HEAD
-import '../styles/dashboardnyacss.css'
-=======
-import '../styles/layLogin.css'
 import '../styles/dashboard.css'
->>>>>>> 249d99f5029f3b26b412e1f9ad313ac6460a8fdb
 import Link from 'next/link'
 import Head from 'next/head'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-
 
 // function MyApp()
 function MyApp({ Component, pageProps }) {
@@ -21,9 +15,11 @@ function MyApp({ Component, pageProps }) {
     <>
     <Head>
         <title>MyPage</title>
-        <meta name="description" content="Free tutorials on web development" />
-        {/* <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet" />
-        <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'></link> */}
+        <meta name="description" content="Shorten-Link Unjaya" />
+               {/* {/* <link
+          href="https://fonts.googleapis.com/css?family=Ubuntu+Mono&display=swap"
+          rel="stylesheet"
+        /> */}
     </Head>
     <Header/>
          <Component {...pageProps} />
