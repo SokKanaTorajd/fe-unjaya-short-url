89f3eb6e2b976b1dd75ce695947fa02d025f5460
Atiinn <80826110+atinnna@users.noreply.github.com>
2021-12-03 21:34:09 +0700

    update header2.js

--- components/Header2.js
+++ components/Header2.js
@@ -1,15 +1,10 @@
 import Link from 'next/link'
 import Image from 'next/image'
 
-<<<<<<< HEAD
-
 function Header2(){
   function userbin(){
 
   }
-=======
-function Header2(){
->>>>>>> main
     return(
       <>
       <div className="headdash">
@@ -17,11 +12,7 @@ function Header2(){
             <div className="header">
               <h1>Unjaya.id</h1>
             </div>
-<<<<<<< HEAD
             <div className="headernya">
-=======
-            <div className="headernya"> 
->>>>>>> main
               <div className ="dashb">
                 <h2>Dashboard</h2>
               </div>
@@ -29,32 +20,16 @@ function Header2(){
                 <Image src="/Group.png" alt="akun" width={23} height={23}/>
               </div>
               <div className="poly">
-<<<<<<< HEAD
                 <Image src="/polygon.png" onClick={userbin} alt="poly" width={25} height={8}/>
                 <div className="user">
                   {/* <h3>@Userbin</h3>
                   <p>Keluar</p> */}
-=======
-                <Image src="/polygon.png" alt="poly" width={25} height={8}/>
-                <div className="user">
-                  <h3>@Userbin</h3>
->>>>>>> main
                 </div>
               </div>
             </div>
         </div>
       </div>
   </>
-    // <div className="headernya">
-  
-    //     <div className="header">
-    //       <h1>Unjaya.id</h1>
-    //       <h2>Dashboard</h2>
-    //     </div>
-    //     <Link href='/'>
-    //     <a class="dashboard">dashboard</a> 
-    //     </Link>
-    // </div>
   )
 }
 export default Header2
