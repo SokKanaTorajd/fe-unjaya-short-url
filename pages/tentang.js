f070f72778c6e920c5665c3f18ed9637e4828fdb
Mohammad Yudatama <93419676+MYudatama@users.noreply.github.com>
2022-02-12 23:33:28 +0700

    Update tentang.js
    edit tentang

--- pages/tentang.js
+++ pages/tentang.js
@@ -4,14 +4,16 @@ import Footer from '../components/Footer'
 
 function Tentang(){
     return(
-        <div className='tentang1'>
-            <h1 className='heading'><span>Tentang Kami</span></h1>
-            <div className='abt-1'>
-                <p><br></br>Unjaya.id ShortenLink is the perfect free URL shortener for transforming long, ugly links into nice, 
-                memorable and trackable short URLs.<br></br> Use it to shorten links for any social media platforms,
-                blogs, SMS, emails, ads, or pretty much anywhere else you want to share them.<br></br> 
-                Twitter, Facebook, YouTube, Instagram, WhatsApp, emails, SMS, videos.</p>
-            </div>
+        <div className='container'>
+            <div className='tentang1' id='about'>
+                        <h1 className='heading'><span>Tentang Kami</span></h1>
+                        <div className='abt-1'>
+                            <p><br></br>Unjaya.id ShortenLink is the perfect free URL shortener for transforming long, ugly links into nice, 
+                            memorable and trackable short URLs.<br></br> Use it to shorten links for any social media platforms,
+                            blogs, SMS, emails, ads, or pretty much anywhere else you want to share them.<br></br> 
+                            Twitter, Facebook, YouTube, Instagram, WhatsApp, emails, SMS, videos.</p>
+                        </div>
+                    </div>
         </div>
     )}
 
