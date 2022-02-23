61d9873d4f903a42abf2bf5a8c3827dad959b751
dennisfitri <202103002@ftti.unjaya.ac.id>
2022-02-23 11:05:23 +0700

    popup userbin

--- components/Header2.js
+++ components/Header2.js
@@ -1,17 +1,36 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import React from "react";
 
-function Header2(){
-  function userbin(){
-      const userbin = document.getElementById('userbin')
-      if(userbin.style.display === 'none'){
-        userbin.style.display='block'
-      }
-      else{
-        userbin.style.display='none'
-      }
-      
-  }
+  class Header2 extends React.Component {
+    container = React.createRef();
+    state = {
+      open: false,
+    };
+    handleButtonClick = () => {
+      this.setState((state) => {
+        return {
+          open: !state.open,
+        };
+      });
+    };
+    componentDidMount() {
+        document.addEventListener("mousedown", this.handleClickOutside);
+    }
+    componentWillUnmount() {
+      document.removeEventListener("mousedown", this.handleClickOutside);
+    }
+    handleClickOutside = (event) => {
+        if (
+          this.container.current &&
+          !this.container.current.contains(event.target)
+        ) {
+          this.setState({
+            open: false,
+          });
+        }
+      };
+render(){
     return(
       <>
       <div className="headdash">
@@ -26,12 +45,15 @@ function Header2(){
                 <div className="dashb">
                   <h2>Wijatama Diwangkara</h2>
                 </div>
-                <div className="poly" onClick={userbin}>
+                <div className="poly"  ref={this.container}>
+                  <button type='button' className='buttonnn' onClick={this.handleButtonClick}>
                   <Image src="/polygon.png" alt="poly" width={25} height={8}/>
+                  </button>
                 </div>
             </div>
         </div>
       </div>
+      {this.state.open &&(
       <div className="userbin" id='userbin'>
           <p>
           <Image src="/dasbor.png"alt="akun" width={16} height={16} padding-right={10} />
@@ -53,8 +75,9 @@ function Header2(){
           </Link>
           </p>
       </div>
-  </>
-   
-  )
+      )}
+  </>  
+  );}
 }
-export default Header2
+
+export default Header2;
