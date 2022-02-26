8270291b39b7397504b9111a0d64d37aaa1ab335
Dewi Arumsari <dewiarumsari38@gmail.com>
2022-02-27 00:29:43 +0700

    update header2 revisi

--- components/Header2.js
+++ components/Header2.js
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 import React from "react";
 
+
   class Header2 extends React.Component {
     container = React.createRef();
     state = {
@@ -45,37 +46,36 @@ render(){
                 <div className="dashb">
                   <h2>Wijatama Diwangkara</h2>
                 </div>
-                <div className="poly"  ref={this.container}>
-                  <button type='button' className='buttonnn' onClick={this.handleButtonClick}>
-                  <Image src="/polygon.png" alt="poly" width={25} height={8}/>
+                <div className="poly" ref={this.container}>
+                  <button type='button' className='buttonpol' onClick={this.handleButtonClick}>
+                  <Image src="/polygon.png" alt="poly" width={25} height={8} />
                   </button>
-                </div>
+                  {this.state.open &&(
+                  <div className="userbinn" id='userbin'>
+                     <p>
+                      <Image src="/dasbor.png"alt="akun" width={18} height={18} padding-right={10} objectFit="contain"/>
+                       <Link href='/dashboard'>
+                           <a className="btn-dashboard">  Dasboard</a>
+                       </Link>
+                     </p>
+                     <p>
+                       <Image src="/pengaturan.png"alt="akun" width={18} height={18} padding-right={10} />
+                       <Link href='/setting'>
+                           <a className="btn-setting">  Pengaturan</a> 
+                       </Link>
+                     </p>
+                     <p>
+                     <Image src="/out.png"alt="akun" width={18} height={18} padding-right={10} />
+                     <Link href='/index'>
+                         <a class='out'>  Keluar</a>
+                     </Link>
+                     </p> 
+                  </div>
+                  )}
+              </div> 
             </div>
         </div>
       </div>
-      {this.state.open &&(
-      <div className="userbin" id='userbin'>
-          <p>
-          <Image src="/dasbor.png"alt="akun" width={16} height={16} padding-right={10} />
-            <Link href='/dashboard'>
-                <a className="btn-dashboard">  Dasboard</a> 
-            </Link>
-          </p>
-
-          <p>
-            <Image src="/pengaturan.png"alt="akun" width={16} height={16} padding-right={10} />
-            <Link href='/setting'>
-                <a className="btn-setting">  Pengaturan</a> 
-            </Link>
-          </p>
-          <p>
-          <Image src="/out.png"alt="akun" width={14} height={14} padding-right={10} />
-          <Link href='/index'>
-              <a>  Keluar</a>
-          </Link>
-          </p>
-      </div>
-      )}
   </>  
   );}
 }
