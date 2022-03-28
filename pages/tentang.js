e49b8677aba97d8b9cb99e5f8edbc5975592a38a
dennisfitri <202103002@ftti.unjaya.ac.id>
2022-03-29 01:27:26 +0700

    revisi paragraf

--- pages/tentang.js
+++ pages/tentang.js
@@ -8,9 +8,9 @@ function Tentang(){
             <div className='tentang1' id='about'>
                         <h1 className='heading'><span>Tentang Kami</span></h1>
                         <div className='abt-1'>
-                            <p><br></br>Unjaya.id ShortenLink is the perfect free URL shortener for transforming long, ugly links into nice, 
-                            memorable and trackable short URLs.<br></br> Use it to shorten links for any social media platforms,
-                            blogs, SMS, emails, ads, or pretty much anywhere else you want to share them.<br></br> 
+                            <p>Unjaya.id ShortenLink is the perfect free URL shortener for transforming long, ugly links into nice, 
+                            memorable and trackable short URLs. Use it to shorten links for any social media platforms,
+                            blogs, SMS, emails, ads, or pretty much anywhere else you want to share them.
                             Twitter, Facebook, YouTube, Instagram, WhatsApp, emails, SMS, videos.</p>
                         </div>
                     </div>
